dbe68cde19dfe78eea84b9d9a9bdf6147b48035d
vamsinallagorla <vamsinallagorla7@gmail.com>
2026-07-15 11:11:05 +0530

    updated user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,26 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    timestamps:true,
-});
-const User=mongoose.model("User",userSchema);
-module.exports=User;
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
