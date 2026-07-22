ec9e8223687b40a640e9cbfd6aa30313a2595a4d
vamsinallagorla <vamsinallagorla7@gmail.com>
2026-07-22 10:30:01 +0530

    updated mubile responsive

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "http://172.20.10.4:5000/api",
 });
 
 api.interceptors.request.use((config) => {
